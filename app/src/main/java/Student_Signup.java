a772bbacc48e43e5efaeb0b720bc57b33b6fead8
Win-10 <yadavashutosh2193@gmail.com>
2018-03-23 16:36:59 +0530

    okay

---
+++ app/src/main/java/Student_Signup.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Win-10 on 3/23/2018.
+ */
+
+public class Student_Signup {
+}
---
+++ app/src/main/java/demoproject/ashutosh/com/student_attendance/Student_Module.java
@@ -0,0 +1,13 @@
+package demoproject.ashutosh.com.student_attendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Student_Module extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student__module);
+    }
+}
